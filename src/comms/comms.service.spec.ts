c60342f8780cba6803034283d0ef3e7c2ce728e2
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-24 17:57:27 +0000

    Describe title test block

--- src/comms/comms.service.spec.ts
+++ src/comms/comms.service.spec.ts
@@ -15,4 +15,29 @@ describe('CommsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('title', () => {
+    test('formatted correctly with 1 cat', () => {
+      const response = service.getNextDelivery(
+        '618f4ed6-1c5b-4993-a149-f64700bf31dd',
+      );
+      expect(response.title).toEqual('Your next delivery for Betsy');
+    });
+    test('formatted correctly with 2 cats', () => {
+      const response = service.getNextDelivery(
+        'c1307701-fe57-4be6-bdc5-184700d69f4d',
+      );
+      expect(response.title).toEqual(
+        'Your next delivery for Corrine and Jessica',
+      );
+    });
+    test('formatted correctly with 3 cats', () => {
+      const response = service.getNextDelivery(
+        '76d6eb8d-5c2e-49f7-b798-d69700dda4c3',
+      );
+      expect(response.title).toEqual(
+        'Your next delivery for Destiny, Tiffany and Alexandre',
+      );
+    });
+  });
 });
