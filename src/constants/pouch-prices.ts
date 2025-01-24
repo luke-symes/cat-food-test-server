227d44a04ea904ccad17a6ce540c87bc76c40324
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-24 16:44:10 +0000

    Ignore formatting in prices

--- src/constants/pouch-prices.ts
+++ src/constants/pouch-prices.ts
@@ -1,10 +1,12 @@
 import { PouchSize } from 'src/types/delivery';
 
+// prettier-ignore
+
 export const POUCH_PRICES: Record<PouchSize, number> = {
-  A: 55.5,
-  B: 59.5,
+  A: 55.50,
+  B: 59.50,
   C: 62.75,
-  D: 66.0,
-  E: 69.0,
+  D: 66.00,
+  E: 69.00,
   F: 71.25,
 } as const;
