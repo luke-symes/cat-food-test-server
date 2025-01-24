c80df149994a8a45c8e08908e77b838d534b842b
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-24 14:25:45 +0000

    Configure comms controller with service & service getter

--- src/comms/comms.controller.ts
+++ src/comms/comms.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CommsService } from 'src/comms/comms.service';
+import { Delivery } from 'src/comms/types/delivery';
 
 @Controller('comms')
-export class CommsController {}
+export class CommsController {
+  constructor(private commsService: CommsService) {}
+
+  @Get('your-next-delivery/:userId')
+  getNextDelivery(@Param() params: { userId: string }): Delivery {
+    return this.commsService.getNextDelivery(params.userId);
+  }
+}
