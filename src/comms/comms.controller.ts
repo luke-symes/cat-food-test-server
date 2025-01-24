5e93c62b24f15c12d3a4a0f848a7f854416185d9
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-24 15:03:43 +0000

    Refactor response type

--- src/comms/comms.controller.ts
+++ src/comms/comms.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CommsService } from 'src/comms/comms.service';
-import { Delivery } from 'src/comms/types/delivery';
+import { DeliveryResponse } from 'src/comms/types/delivery-response';
 
 @Controller('comms')
 export class CommsController {
   constructor(private commsService: CommsService) {}
 
   @Get('your-next-delivery/:userId')
-  getNextDelivery(@Param() params: { userId: string }): Delivery {
+  getNextDelivery(@Param() params: { userId: string }): DeliveryResponse {
     return this.commsService.getNextDelivery(params.userId);
   }
 }
